f8696b1628ffdd0eb257327599ff05fe915bf65f
Roland Chelwing <jolleee@hotmail.com>
2025-06-01 09:22:24 +0200

    fix: manifest

--- src/manifest.ts
+++ src/manifest.ts
@@ -22,7 +22,7 @@ export default defineManifest({
   ],
   web_accessible_resources: [
     {
-      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/logo-128.png'],
+      resources: ['img/logo-16.png', 'img/logo-32.png', 'img/logo-48.png', 'img/logo-64.png', 'img/logo-128.png'],
       matches: [],
     },
   ],
